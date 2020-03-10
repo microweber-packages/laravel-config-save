ed2a5a9b59880aac72d062e68239ae4ac7572a53
b.slaveykov <b.slaveykov@radioactive.bg>
2020-03-10 11:23:57 +0200

    update

---
+++ tests/BaseTest.php
@@ -0,0 +1,28 @@
+<?php
+
+abstract class BaseTest extends Orchestra\Testbench\TestCase
+{
+
+    public function tearDown(): void
+    {
+        \Mockery::close();
+    }
+
+
+    protected function getPackageProviders($app)
+    {
+        return [\MicroweberPackages\Config\ConfigSaveServiceProvider::class];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('app.key', 'tQbgKF5NH5zMyGh4vCNypFAzx9trCkE6x');
+    }
+
+}
---
+++ tests/ConfigSaveTest.php
@@ -0,0 +1,12 @@
+<?php
+
+class ConfigSaveTest extends BaseTest
+{
+    public function testSimple()
+    {
+
+
+
+    }
+
+}
